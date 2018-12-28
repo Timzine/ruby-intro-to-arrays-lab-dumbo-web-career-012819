3b24d2568bb5675b9d5dfa0e27785cb19a72c463
Lemrabt Boudadya <lboudadya@gmail.com>
2018-12-28 18:50:06 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,15 @@
+def instantiate_new_array
+  instantiate_new_array = []
+end
+
+def array_with_two_elements
+  array_with_two_elements = ["one", "two"]
+end 
+
+def first_element(array)
+  array[0] 
+end
+
+def third_element(array)
+  array[2] 
+end
